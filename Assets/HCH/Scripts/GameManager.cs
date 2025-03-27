a51a2c6b71d16acfcee1914e105abad8a3da2f90
hchee99 <hchee99@naver.com>
2025-03-27 20:11:19 +0900

    [Add]json으로 저장

--- Assets/HCH/Scripts/GameManager.cs
+++ Assets/HCH/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor.Experimental.RestService;
 using UnityEngine;
 
@@ -11,22 +12,38 @@ public class GameManager : MonoBehaviour
     public Stat stat;
     public GoldManager goldManager;
 
+    private string savePath;
 
     private void Awake()
     {
         Instance = this;
+        savePath = Path.Combine(Application.persistentDataPath,"save.json");
     }
 
     public void NewGame()
     {
-
+        playerData = new PlayerData()
+        {
+            stage = 1,
+            gold = 0
+        };
+        SaveGame();
     }
     public void LoadGame()
     {
-
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            playerData = JsonUtility.FromJson<PlayerData>(json);
+        }
+        else
+        {
+            NewGame();
+        }
     }
     public void SaveGame()
     {
-
+        string json = JsonUtility.ToJson(playerData,true);
+        File.WriteAllText(savePath, json);
     }
 }
