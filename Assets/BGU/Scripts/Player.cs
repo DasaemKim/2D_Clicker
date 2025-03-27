d7d7cdeb12653349baa6e5cc74d892639dd93ea5
100ZADA <5528asd@gmail.com>
2025-03-27 15:45:03 +0900

    [Chore] 필요 없는 파일 삭제

--- Assets/BGU/Scripts/Player.cs
+++ Assets/BGU/Scripts/Player.cs
@@ -2,19 +2,12 @@
     using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class player : MonoBehaviour
 {
     // 캐릭터 데이터
     public CharacterData characterData;
 
-    private void Start()
-    {
-        Debug.Log(characterData.damage);
-        Debug.Log(characterData.criDamage);
-        Debug.Log(characterData.coinPoint);
-        Debug.Log(characterData.levelPoint);
-        Debug.Log(characterData.autoAtk);
-        Debug.Log(characterData.coinGet);
-    }
+    // 크리티컬 계산
 }
--- Assets/BGU/Scripts/StatUpgrade.cs
+++ Assets/BGU/Scripts/StatUpgrade.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 플레이어 스텟 업그레이드 로직 구성
 public class StatUpgrade : MonoBehaviour
 {
-    // 플레이어 스텟 업그레이드 로직 구성
+    CharacterData characterData;
+    
+    // 데미지 증가
+    public void DamageUpgrade()
+    {
+    }
+
+    // 크리티컬 확률 증가
+    public void CriDamageUpgrade()
+    {
+    }
+
+    // 자동 공격력 증가
+    public void autoAtkUpgrade()
+    {
+    }
+
+    // 코인 획득량 증가
+    public void coinGetUpgrade()
+    {
+    }
 }
