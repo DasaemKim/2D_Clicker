eaf7261fc6479169aa81ae9fde5352f8a5099283
kimhakdo <plota0908@naver.com>
2025-03-28 11:29:51 +0900

    [Feat] ClickSystem 추가
    UI제외 공격 Debug출력

--- Assets/Scripts_KHD/AttackSystem.cs
+++ Assets/Scripts_KHD/AttackSystem.cs
@@ -1,26 +1,30 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class AttackSystem : MonoBehaviour
 {
-    void Update()
-    {
-        
-    }
 
     public void Attack()
-    { 
-    
-    }
+    {
+        Debug.Log("공격 실행!");
 
-    public void CriticalAttack()
-    { 
-    
     }
 
     public void AutoAttack()
-    { 
-    
+    {
+        Debug.Log("자동공격 실행!");
+    }
+
+    public void Critical()
+    {
+        Debug.Log("크리티컬 공격 실행!");
+    }
+
+    public void AttackEffect()
+    {
+        Debug.Log("공격 이펙트 실행!");
     }
 }
