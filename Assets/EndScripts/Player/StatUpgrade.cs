1801abe1c626e082cec5daadee0712cb30f9c76c
100ZADA <5528asd@gmail.com>
2025-03-28 21:00:49 +0900

    [Docs] 스크립트 주석 추가

--- Assets/EndScripts/Player/StatUpgrade.cs
+++ Assets/EndScripts/Player/StatUpgrade.cs
@@ -9,6 +9,7 @@ public class StatUpgrade : MonoBehaviour
     // CharacterData의 주소값 없음
     CharacterData characterData;
 
+    // 버튼의 스탯 포인트 기본값
     public float criUpgradePoint;
     public float autoUpgradePoint;
     public float coinUpgradePoint;
@@ -19,28 +20,30 @@ public class StatUpgrade : MonoBehaviour
         characterData = GetComponent<Player>().characterData;
     }
     
-    //TODO
+    //TODO 진행 중
     // 현재 강화 시 값은 올라가나 프로그램 종료 후 다시 키면 초기 값으로 돌아감.
     // 내일 UI텍스트를 추가하여 RefreshUI 메서드를 통하여 나가더라도 값이 고정되도록 변경
+    // PlayerData.cs에 데이더 저장 진행
     
-    // TODO
+    // TODO 3월 31일에 작성
     // 마우스 키 다운시 0.2초 간격으로 강화 하는거 만들기
     // 아마도 input.MounseButtonDown을 이용하여 작성
 
     // 크리티컬 데미지 증가
     public void CriDamageUpgrade()
     {
+        // 플레이어의 스탯 포인트가 강화 스탯 포인트 보다 클때
         if (characterData.statPoint >= criUpgradePoint)
         {
-            characterData.statPoint -= (int)criUpgradePoint;
-            characterData.criDamage = 0.1f + characterData.criDamage * 1.5f;
-            criUpgradePoint *= 1.5f;
+            characterData.statPoint -= (int)criUpgradePoint;    // 업그레이드 포인트 만큼 스탯 포인트에서 값 감소
+            characterData.criDamage = 0.1f + characterData.criDamage * 1.5f;  // 스탯 포인트 증가값
+            criUpgradePoint *= 1.5f;   // 강화 포인트 사용 시 다음 사용할 때 1.5배 추가 증가
 
-            UIBtnManager.Instance.uiBtnController.RefreshUI();
+            UIBtnManager.Instance.uiBtnController.RefreshUI(); // 업그레이드 수치 변경 시 최신화
         }
         else
         {
-            UIBtnManager.Instance.uiBtnController.ErrorPanel();
+            UIBtnManager.Instance.uiBtnController.ErrorPanel(); // 에러 발생 시 에러 패널 생성
             StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
@@ -48,17 +51,18 @@ public class StatUpgrade : MonoBehaviour
     // 자동 공격 횟수 증가
     public void AutoNumUpgrade()
     {
+        // 플레이어의 스탯 포인트가 강화 스탯 포인트 보다 클때
         if (characterData.statPoint >= autoUpgradePoint)
         {
-            characterData.statPoint -= (int)autoUpgradePoint;
-            characterData.autoNum = 1 + characterData.autoNum + 0.3f;
-            autoUpgradePoint *= 1.5f;
+            characterData.statPoint -= (int)autoUpgradePoint;    // 업그레이드 포인트 만큼 스탯 포인트에서 값 감소
+            characterData.autoNum = 1 + characterData.autoNum + 0.3f;  // 스탯 포인트 증가값
+            autoUpgradePoint *= 1.5f;   // 강화 포인트 사용 시 다음 사용할 때 1.5배 추가 증가
 
-            UIBtnManager.Instance.uiBtnController.RefreshUI();
+            UIBtnManager.Instance.uiBtnController.RefreshUI(); // 업그레이드 수치 변경 시 최신화
         }
         else
         {
-            UIBtnManager.Instance.uiBtnController.ErrorPanel();
+            UIBtnManager.Instance.uiBtnController.ErrorPanel(); // 에러 발생 시 에러 패널 생성
             StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
@@ -66,17 +70,18 @@ public class StatUpgrade : MonoBehaviour
     // 코인 획득량 증가
     public void CoinGetUpgrade()
     {
+        // 플레이어의 스탯 포인트가 강화 스탯 포인트 보다 클때
         if (characterData.statPoint >= coinUpgradePoint)
         {
-            characterData.statPoint -= (int)coinUpgradePoint;
-            characterData.coinGet = 0.1f + characterData.coinGet * 2f;
-            coinUpgradePoint *= 1.5f;
+            characterData.statPoint -= (int)coinUpgradePoint;    // 업그레이드 포인트 만큼 스탯 포인트에서 값 감소
+            characterData.coinGet = 0.1f + characterData.coinGet * 2f;  // 스탯 포인트 증가값
+            coinUpgradePoint *= 1.5f;   // 강화 포인트 사용 시 다음 사용할 때 1.5배 추가 증가
 
-            UIBtnManager.Instance.uiBtnController.RefreshUI();
+            UIBtnManager.Instance.uiBtnController.RefreshUI(); // 업그레이드 수치 변경 시 최신화
         }
         else
         {
-            UIBtnManager.Instance.uiBtnController.ErrorPanel();
+            UIBtnManager.Instance.uiBtnController.ErrorPanel(); // 에러 발생 시 에러 패널 생성
             StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
