4fc8ec8d5b2e0b7cd4f83e09831f04a3351f1b84
100ZADA <5528asd@gmail.com>
2025-03-28 11:50:01 +0900

    [Feat] 패널 기능 추가
    # UIButtonController.cs
     - Coroutine을 이용하여 1 초후 패널 사라짐

--- Assets/BGU/Scripts/StatUpgrade.cs
+++ Assets/BGU/Scripts/StatUpgrade.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // 골드를 소모하여 플레이어 스탯 업그레이드 로직 구성
@@ -7,9 +8,14 @@ public class StatUpgrade : MonoBehaviour
 {
     // CharacterData의 주소값 없음
     CharacterData characterData;
-    
-    int statUpgrade = 10;
-    
+
+    private float pointTime = 0.2f; // 0.2초마다 실행
+    private float nextTime = 0.0f; // 다음번 실행할 시간
+
+    public int criUpgradePoint;
+    public int autoUpgradePoint;
+    public int coinUpgradePoint;
+
     void Start()
     {
         // 힙 메모리 영역의 주소값을 가져옴
@@ -17,66 +23,63 @@ public class StatUpgrade : MonoBehaviour
     }
     
     //TODO
-    // 현재 강화 시 값은 올라가나 껏다 키면 초기 값으로 돌아감.
+    // 현재 강화 시 값은 올라가나 프로그램 종료 후 다시 키면 초기 값으로 돌아감.
     // 내일 UI텍스트를 추가하여 RefreshUI 메서드를 통하여 나가더라도 값이 고정되도록 변경
     // 그리고 마우스 키 다운시 0.2초 간격으로 강화 하는거 만들기
     // 아마도 input.MounseButtonDown을 이용하여 작성 가능
     //
-    
-    // 크리티컬 확률 증가
+
+    // 크리티컬 데미지 증가
     public void CriDamageUpgrade()
     {
-        if (characterData.statPoint > statUpgrade)
+        if (characterData.statPoint >= criUpgradePoint)
         {
-            characterData.statPoint -= statUpgrade;
+            characterData.statPoint -= criUpgradePoint;
             characterData.criDamage = 0.1f + characterData.criDamage * 1.5f;
-            statUpgrade *= 2;
-            
+            criUpgradePoint *= 2;
+
             UIBtnManager.Instance.uiBtnController.RefreshUI();
-            Debug.Log(characterData.criDamage);
         }
         else
         {
             UIBtnManager.Instance.uiBtnController.ErrorPanel();
-            Debug.Log("골드가 부족합니다.");
+            StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
 
     // 자동 공격 횟수 증가
     public void AutoNumUpgrade()
     {
-        if (characterData.statPoint > statUpgrade)
+        if (characterData.statPoint >= autoUpgradePoint)
         {
-            characterData.statPoint -= statUpgrade;
+            characterData.statPoint -= autoUpgradePoint;
             characterData.autoNum = 1 + characterData.autoNum + 0.3f;
-            statUpgrade *= 2;
-            
+            autoUpgradePoint *= 2;
+
             UIBtnManager.Instance.uiBtnController.RefreshUI();
-            Debug.Log(characterData.autoNum);
         }
         else
         {
             UIBtnManager.Instance.uiBtnController.ErrorPanel();
-            Debug.Log("골드가 부족합니다.");
+            StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
 
     // 코인 획득량 증가
     public void CoinGetUpgrade()
-    {    
-        if (characterData.statPoint > statUpgrade)
-        {   
-            characterData.statPoint -= statUpgrade;
-            characterData.coinGet = 0.1f + characterData.coinGet * 2f;  
-            statUpgrade *= 2;
-            
+    {
+        if (characterData.statPoint >= coinUpgradePoint)
+        {
+            characterData.statPoint -= coinUpgradePoint;
+            characterData.coinGet = 0.1f + characterData.coinGet * 2f;
+            coinUpgradePoint *= 2;
+
             UIBtnManager.Instance.uiBtnController.RefreshUI();
-            Debug.Log(characterData.coinGet);
         }
         else
         {
             UIBtnManager.Instance.uiBtnController.ErrorPanel();
-            Debug.Log("골드가 부족합니다.");
+            StartCoroutine(UIBtnManager.Instance.uiBtnController.CloseErrorPanel()); // 코루틴을 이용하여 3초뒤 사라짐
         }
     }
 }
--- Assets/BGU/Scripts/UIButtonController.cs
+++ Assets/BGU/Scripts/UIButtonController.cs
@@ -3,29 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class UIButtonController : MonoBehaviour
 {
-    [Header("UI 버튼")] public Button criBtn;
+    [Header("UI 버튼")]
+    public Button criBtn;
     public Button autoNumBtn;
     public Button coinGetBtn;
 
-    [Header("UI 재화")] public TextMeshProUGUI weaponText;
+    [Header("UI 재화")]
+    public TextMeshProUGUI weaponText;
     public TextMeshProUGUI statText;
+    public TextMeshProUGUI criInfoText; // 현재 크리 강화 수치
+    public TextMeshProUGUI autoInfoText; // 현재 자동 공격 강화 수치
+    public TextMeshProUGUI coinInfoText; // 현재 코인 획득 강화 수치
+    public TextMeshProUGUI criUpgradeText; // 현재 강화 표시
+    public TextMeshProUGUI autoUpgradeText; // 현재 강화 표시
+    public TextMeshProUGUI coinUpgradeText; // 현재 강화 표시
 
     [Header("UI 패널")]
     public GameObject errorPanel;
 
     public Player player;
+    public StatUpgrade statUpgrade;
 
     private void Start()
     {
         criBtn.onClick.AddListener(OnClickCriBtn);
         autoNumBtn.onClick.AddListener(OnClickAutoNumBtn);
         coinGetBtn.onClick.AddListener(OnClickCoinGetBtn);
-        
+
         RefreshUI();
     }
 
@@ -49,6 +57,14 @@ public class UIButtonController : MonoBehaviour
     {
         weaponText.text = player.characterData.weaponPoint.ToString("N0");
         statText.text = player.characterData.statPoint.ToString("N0");
+
+        criInfoText.text = player.characterData.criDamage.ToString("N2") + " %";
+        autoInfoText.text = player.characterData.autoNum.ToString("N1") + " 초/회";
+        coinInfoText.text = player.characterData.coinGet.ToString("N2") + " %";
+
+        criUpgradeText.text = statUpgrade.criUpgradePoint.ToString();
+        autoUpgradeText.text = statUpgrade.autoUpgradePoint.ToString();
+        coinUpgradeText.text = statUpgrade.coinUpgradePoint.ToString();
     }
 
     // ErrorPanel 패널 생성
@@ -56,14 +72,14 @@ public class UIButtonController : MonoBehaviour
     {
         errorPanel.SetActive(true);
     }
-    
+
     // TODO
-    // 에러 패널이 3 ~ 5초 뒤에 사라지도록 설정
-    //
-    
-    // CloseErrorPanel 패널 생성
-    public void CloseErrorPanel()
+    // 에러 패널이 3초 뒤에 사라지도록 설정
+    // 코루틴을 이용하여 1초뒤 사라지도록 설정
+    // ErrorPanel 패널 사라짐
+    public IEnumerator CloseErrorPanel()
     {
+        yield return new WaitForSeconds(1);
         errorPanel.SetActive(false);
     }
 }
