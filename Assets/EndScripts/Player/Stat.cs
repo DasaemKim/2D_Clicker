3fd536952f1e5c98de088599f7b3ea16ee46d2a8
hchee99 <hchee99@naver.com>
2025-03-31 14:40:30 +0900

    [Fix]치명타 데미지 식 변경

--- Assets/EndScripts/Player/Stat.cs
+++ Assets/EndScripts/Player/Stat.cs
@@ -7,25 +7,24 @@ using UnityEngine;
 public class Stat
 {
     public int baseAttack;
-    public int baseCriticalRate;
+    public float baseCriticalRate;
     public int baseCriticalDamage;
     public int baseGoldBonus;
 
     public int FinalAttack(PlayerData playerData, Weapon.WeaponList weapon)
     {
-        return baseAttack+weapon.Attack;
+        return baseAttack + weapon.Attack;
     }
     public float FinalCriticaRate(PlayerData playerData, Weapon.WeaponList weapon)
     {
         return baseCriticalRate + weapon.Critical;
     }
-    public int FinalCriticalDamage(PlayerData playerData, Weapon.WeaponList weapon)
+    public int FinalCriticalDamage(PlayerData playerData)
     {
-        int upgradeBonus = 50 * playerData.criticalDamageLevel;
-        return Mathf.RoundToInt(weapon.Critical_Damage * 100) + upgradeBonus;
+        return baseCriticalDamage + (50 * playerData.criticalDamageLevel);
     }
     public int FinalGoldBonus(PlayerData playerData)
     {
-        return 100 * playerData.goldBonusLevel;
+        return baseGoldBonus + (100 * playerData.goldBonusLevel);
     }
 }
